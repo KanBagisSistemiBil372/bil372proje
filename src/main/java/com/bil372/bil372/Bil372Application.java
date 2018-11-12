87046256163e299e67f920a00ba4933cbe8903ad
AGS <ahmetgokhansener@gmail.com>
2018-11-12 23:52:27 +0300

    First commit

---
+++ src/main/java/com/bil372/bil372/Bil372Application.java
@@ -0,0 +1,12 @@
+package com.bil372.bil372;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bil372Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bil372Application.class, args);
+	}
+}
