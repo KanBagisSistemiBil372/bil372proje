710b8d7ed4b9a1b3a41f3b8539bd2d5d891eb5fb
ipekakova <iakova97@gmail.com>
2018-11-21 16:14:49 +0300

    Login Controller eklendi

--- src/main/java/com/bil372/bil372/controller/DenemeController.java
+++ src/main/java/com/bil372/bil372/controller/DenemeController.java
@@ -5,6 +5,9 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.ui.Model;
+
 
 @Controller
 public class DenemeController {
@@ -12,6 +15,17 @@ public class DenemeController {
     @RequestMapping("/")
     public String welcome() {
         return "homepage";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(Model model, String error, String logout) {
+        if (error != null)
+            model.addAttribute("error", "Your username and password is invalid.");
+
+        if (logout != null)
+            model.addAttribute("message", "You have been logged out successfully.");
+
+        return "login";
     }
 
 
